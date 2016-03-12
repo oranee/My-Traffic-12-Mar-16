530ec435eba36057be6b034e01ad13d08ad9bae9
masterUNG <phrombutr@gmail.com>
2016-03-12 14:44:24 +0700

    second round11

--- app/src/main/java/com/meng/hlinjue/mytraffic/MainActivity.java
+++ app/src/main/java/com/meng/hlinjue/mytraffic/MainActivity.java
@@ -1,13 +1,48 @@
 package com.meng.hlinjue.mytraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends ActionBarActivity {
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutmeButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+    } // main method
+
+    private void buttonController() {
+        aboutmeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Sound effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.elephant);
+                mediaPlayer.start();
+
+            }//onclick
+        });
     }
-}
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutmeButton = (Button) findViewById(R.id.button);
+    }
+} // Main class !!
